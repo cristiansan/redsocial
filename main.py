5a9ca6dc2a3e3829179533e9601a6821e7665339
San Emeterio <c.san.emeterio@accenture.com>
2019-11-25 18:57:37 -0300

    arreglos post validaciones

--- main.py
+++ main.py
@@ -9,36 +9,41 @@ os.system('cls')
 
 def menu_usuario(usuario):
     # print(usuario.get_email())#esto es una prueba para mostrar email
-    os.system('cls')
-    opcion=input(" 1. Ver Post\n 2. Agregar un nuevo Post\n 3. Borrar Post\n 4. Ver Amigos\n 5. Agregar Amigo\n 6. Eliminar Amigo\n 7. Cerrar Sesión\n ")
-
+    # os.system('cls')
+    # print:("\nMenu:\n")
+    
+    opcion=input("\nMenu:\n=====\n 1. Ver Post\n 2. Agregar un nuevo Post\n 3. Borrar Post\n 4. Ver Amigos\n 5. Agregar Amigo\n 6. Eliminar Amigo\n 7. Cerrar Sesión\n ")
+    os.system('cls')                    #Pedido de Anthony para poner
     if opcion=="1":
         categorias=db.mostrar_categoria()
         for i in categorias:
-            print(i)
+            # print(i)
+            print(f"{i[0]}. {i[1]}")    #Pedido de Anthony para poner
         cat=input("Indique categoría: ")
         # id=db.consultar_id(usuario.get_email())
         # print(id[0][0])       #esto me trae el ID del usuario
         posteos_cat=db.list_posteo_categoria(cat)
+        print(f"Estos son tus posts en la categoría {cat}:\n")
         for i in posteos_cat:
             print(i[0])
-            print()
+            # print()
         # post=input("Ingrese posteo: ")
         # db.add_posteo(post, id[0][0], cat) 
-        input("Presione Enter para continuar...")
+        input("\nPresione Enter para continuar...")
         menu_usuario(usuario)
-    
+
     elif opcion=="2":
         categorias=db.mostrar_categoria()
         for i in categorias:
-            print(i)
+            # print(i)
+            print(f"{i[0]}. {i[1]}")    #Pedido de Anthony para poner        
         cat=input("Indique categoría: ")
         id=db.consultar_id(usuario.get_email())
         print(id[0][0])
         post=input("Ingrese posteo: ")
         db.add_posteo(post, id[0][0], cat)
         print("Posteo agregado!")
-        input("Presione Enter para continuar...")
+        input("\nPresione Enter para continuar...")
         # os.system('cls')
         menu_usuario(usuario)
 
@@ -54,38 +59,40 @@ def menu_usuario(usuario):
     elif opcion =="4":
         id=db.consultar_id(usuario.get_email())
         amigos=db.ver_amigos(id[0][0])
-        print("Tus amigos son:")
+        if amigos==[]:
+            print("No tenes amigos, looser!!")
+        else:
+            print("Este es el listado de tus amigos:")
         for i in amigos:
             mostrar=(db.mostrar_amigos(i[0]))
             print(mostrar[0][0], mostrar[0][1])
-        input("Presione Enter para continuar...")
+        input("\nPresione Enter para continuar...")
         menu_usuario(usuario)
-
-        
+      
     elif opcion=="5":
         amigo=input("Indica el email de tu amigo: ")
         if db.validar_amigo(amigo) == []:
         	print("Usuario no existe")
-            # menu_usuario(usuario)
         else:
             db.agregar_amigo(usuario, amigo)
             print("Amigo agregado!! :)")
-         # falta poner mensaje si existe o no.
         menu_usuario(usuario)
     
-    elif opcion==6:
+    elif opcion=="6":
         amigo=input("Indica el email de tu amigo: ")
         if db.validar_amigo(amigo) == []:
         	print("Usuario no existe")
         else:
             db.eliminar_amigo(usuario, amigo)
-            menu_usuario(usuario)
+            print("ex-Amigo eliminado!! :(")
+        menu_usuario(usuario)
     else:
         menu_login()
 
 def menu_login():
     os.system('cls')
-    print("Elije Opcion Deseada: ")
+    print("Elije Opcion Deseada:")
+    print("=====================\n")
     valor=input("1.Loguearse\n2.Registrarse\n")
     db=Database()
     if valor=="1":
@@ -94,13 +101,14 @@ def menu_login():
         usuarioglobal=db.login(u, c)
         # os.system('cls')
         if usuarioglobal:
-            print(f"Bienvenido {usuarioglobal[0][1]} {usuarioglobal[0][2]}\n\nEsta es la red social Punk!!\nElige una de estas opciones\n")
+            print(f"\nBienvenido {usuarioglobal[0][1]} {usuarioglobal[0][2]} a la red social Punk!!\n\n----------------------------\nElige una de estas opciones:\n")
             usuario=Usuario(usuarioglobal[0][1], usuarioglobal[0][2], usuarioglobal[0][3], usuarioglobal[0][4], usuarioglobal[0][5], usuarioglobal[0][6], usuarioglobal[0][7], usuarioglobal[0][8])
+            # input("Presiona Enter para continuar...")
             menu_usuario(usuario)
         else:
             print("El usuario no existe, ingrese un usuario válido")
             #pendiente un contador de máximo 3 intentos
-            input("Presione Enter para continuar...")
+            input("\nPresione Enter para continuar...")
             menu_login()
                     
     elif valor=="2":
@@ -119,13 +127,13 @@ def menu_login():
             # if (aca deberiamos hacer la validacion)
             db.crear_usuario(usuario)
             print("Usuario registrado on éxito\ahora vuelve a loguearte por favor\n")
-            input("Presiona Enter para continuar...")
+            input("\nPresiona Enter para continuar...")
             #validar si el usuario ya existe (faltante)
             menu_login()
         else:
             print("El usuario ya existe")
             #pendiente un contador de máximo 3 intentos
-            input("Presione Enter para continuar...")
+            input("\nPresione Enter para continuar...")
             menu_login()
 
     else:
